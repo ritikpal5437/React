9cb2e8a6a2897c8c946f677840c602b370907183
Ritik Pal <ritikpal5437@gmail.com>
2026-09-01 22:35:15 +0530

    cwsdc

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+// Create a React component called Navbar 
+
+import React from "react";
+
+const navbar=()=>{
+    return(
+        <div>
+            <nav>
+                <h1>My React App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export navbar 
