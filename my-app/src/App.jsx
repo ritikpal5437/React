db10ac4215c8c7c566040e5cc3ec22b2f56993d7
Ritik Pal <ritikpal5437@gmail.com>
2026-09-04 16:25:52 +0530

    refactor: clean up App component by removing unused state and effects

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,43 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState ,useRef} from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 
+
+
+// import { useRef } from "react";
+
 function App() {
-  const [count, setCount] = useState(0);
 
-  // useEffect(() => {
-  //   console.log("component rendered");
-  // },[] );
-  useEffect(() => {
-    const interval=setInterval(()=>{
-    console.log('Timer Running');
-    
-    },1000
-  );
-    // console.log("Effect Started");
+  const inputRef = useRef(null);
 
-    return()=>{
-      clearInterval(interval)
-      console.log('cleanup');
-      
-    };
-  },[ count] );
+  function focusInput() {
+    inputRef.current.focus();
+  }
 
   return (
     <>
-      <h1>My React App</h1>
+      <input ref={inputRef} />
 
-      <button onClick={() => setCount(count + 1)}>
-        Count: {count}
+      <button onClick={focusInput}>
+        Focus Input
       </button>
-
-      <Navbar />
-
-      <Card name="Ritik" age={21} course="MCA" />
-      <Card name="Vicky" age={22} course="MCA" />
-      <Card name="Ravit" age={23} course="MCA" />
     </>
   );
 }
 
 export default App;
+
