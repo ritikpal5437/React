5278baf18b53253916bd101140b634c880c2465c
Ritik Pal <ritikpal5437@gmail.com>
2026-09-05 16:03:54 +0530

    refactor: simplify login/logout logic by using conditional statements

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,57 +1,82 @@
+// // import { useState } from "react";
+
+// // function App() {
+// //   const[isloggedIn,setIsLoggedIn]=useState(false);
+// //   return(
+// //     <>
+// //     {/* {
+// //       isloggedIn?(
+// //         <h1>Welcome Ritik</h1>
+// //       ) : (
+// //         <h1>Please Login</h1>   // main line jis se ki pta chlta h user login or logiout ke baare me
+
+// //       )
+// //     } */}
+// //     <button onClick={()=> setIsLoggedIn(!isloggedIn)}>
+// //       login / logout
+// //       </button>
+// //       {isloggedIn  && <h2>Welcom Ritik</h2>} // ise tb use krnge jab sirf ek hi condition true ho or kooi or conditin nahi chiye ho
+// //     </>
+// //   );
+  
+// // }
+// // export default App;
+// // // Ternary:
+
+// // // condition ? A : B
+
+// // // ➡️ True ho to A, false ho to B.
+
+// // // AND:
+
+// // // condition && A
+
+// // // ➡️ True ho to A, false ho to kuch nahi.
+
+
 // import { useState } from "react";
 
 // function App() {
-//   const[isloggedIn,setIsLoggedIn]=useState(false);
-//   return(
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   return (
 //     <>
-//     {/* {
-//       isloggedIn?(
+//       {isLoggedIn ? (
 //         <h1>Welcome Ritik</h1>
 //       ) : (
-//         <h1>Please Login</h1>   // main line jis se ki pta chlta h user login or logiout ke baare me
+//         <h1>Please Login</h1>
+//       )}
 
-//       )
-//     } */}
-//     <button onClick={()=> setIsLoggedIn(!isloggedIn)}>
-//       login / logout
+//       <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+//         Login / Logout
 //       </button>
-//       {isloggedIn  && <h2>Welcom Ritik</h2>} // ise tb use krnge jab sirf ek hi condition true ho or kooi or conditin nahi chiye ho
+
+//       {isLoggedIn && <h2>Welcome Ritik</h2>}
 //     </>
 //   );
-  
 // }
+
 // export default App;
-// // Ternary:
-
-// // condition ? A : B
-
-// // ➡️ True ho to A, false ho to B.
-
-// // AND:
-
-// // condition && A
-
-// // ➡️ True ho to A, false ho to kuch nahi.
-
-
 import { useState } from "react";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  let message;
+
+  if (isLoggedIn) {
+    message = <h1>Welcome Ritik</h1>;
+  } else {
+    message = <h1>Please Login</h1>;
+  }
+
   return (
     <>
-      {isLoggedIn ? (
-        <h1>Welcome Ritik</h1>
-      ) : (
-        <h1>Please Login</h1>
-      )}
+      {message}
 
       <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
         Login / Logout
       </button>
-
-      {isLoggedIn && <h2>Welcome Ritik</h2>}
     </>
   );
 }
