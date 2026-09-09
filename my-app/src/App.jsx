60e378fd3fead543f6a1c708ba33f83e095de6f0
Ritik Pal <ritikpal5437@gmail.com>
2026-09-09 16:02:53 +0530

    refactor: enhance App component with navigation links and structure

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,24 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
-import Home from "./compRouter/Home";
-import About from "./compRouter/About";
+import { BrowserRouter, Routes, Route ,Link} from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+
+function App() {
+  return (
+    <BrowserRouter>
+          <nav>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
