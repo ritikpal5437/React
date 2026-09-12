cd453b1789d526e487213574ca051ed7a3351194
Ritik Pal <ritikpal5437@gmail.com>
2026-09-12 12:21:38 +0530

    feat: implement App component with state management and memoization for square calculation

---
+++ my-app/src/App.jsx
@@ -0,0 +1,28 @@
+import { useMemo,useState } from "react";
+function App() {
+    const[count,setCount]=useState(0);
+    const[number,setNumber]=useState(1);
+
+  
+      const square = useMemo(() => {
+    console.log("Calculation running...");
+    return number * number;
+  }, [number]);
+
+
+return(
+    <>
+    <h1>Count: {count}</h1>
+    <button onClick={()=>setCount(count+1)}>
+    Increase count
+    </button>
+       <h2>Number: {number}</h2>
+      <button onClick={() => setNumber(number + 1)}>
+        Increase Number
+      </button>
+
+    <h2>Square:{square}</h2>
+    </>
+);
+}
+export default App;
